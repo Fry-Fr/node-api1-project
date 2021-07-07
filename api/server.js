c1930ef84816601ecd027358021d6174ee021654
Fry-Fr <ryjlwdg89@gmail.com>
2021-07-07 04:02:30 -0400

    put model update to users/:id

--- api/server.js
+++ api/server.js
@@ -27,6 +27,25 @@ server.get('/api/users/:id', (req, res) => {
     .catch(err => {
         res.status(500).json({message: err.message})
     })
+});
+
+server.put('/api/users/:id', (req, res) => {
+    const changes = req.body;
+    const { id } = req.params;
+
+    if (!changes.name || !changes.bio) {
+        res.status(400).json({message: 'name and bio is required.'})
+    }else{
+        model.update(id, changes)
+        .then(userUpdated => {
+            userUpdated
+            ? res.status(200).json(userUpdated)
+            : res.status(400).json({message: `could not find user id ${id}`})
+        })
+        .catch(err => {
+            res.status(500).json({message: err.message})
+        })
+    }
 })
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
